c2d59a4cdf7e7b725d99391fefc464bc08ac6ff4
Ali Haider <alibarcha910@gmail.com>
2022-09-07 22:44:49 -0700

    initial commit

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+    
+    <header class="sticky bg-wether-primary shadow-lg top-0">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6 ">
+            <router-link :to="{name:'home'}">
+                <div class="flex items-center gap-3 flex-1">
+                    <i class="fa-regular fa-sun"></i>
+                    <p class="text-2xl">The Local Wether</p>
+                </div>
+            </router-link>
+
+            <div class="flex gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+            </div>
+            
+        </nav>
+    </header>
+
+</template>
+
+<script>
+export default {
+    name:'SiteNavigation',
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+      <h2 >home component</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+   
+  }
+}
+</script>
